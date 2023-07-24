c5685b51ebb3a187b25a455476c29d139fb634f9
hanbyeol kwon <janviertrois@naver.com>
2023-07-24 17:16:11 -0400

    Add Navbar and Widgets

--- admin/src/pages/home/Home.js
+++ admin/src/pages/home/Home.js
@@ -1,14 +1,20 @@
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
 import "./home.scss";
 
 function Home() {
   return (
     <div className="home">
       <Sidebar />
-      {/* <Navbar /> */}
       <div className="homeContainer">
-        container
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   )
