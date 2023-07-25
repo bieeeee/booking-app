8c7c6dc068c2371c0b6d190924b81123db582ec2
hanbyeol kwon <janviertrois@naver.com>
2023-07-25 17:33:34 -0400

    Add isAdmin property to res to prevent unallowed access to Admin page

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -24,16 +24,16 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username })
-    if(!user) return next(createError(404, "User not found"))
+    if (!user) return next(createError(404, "User not found"))
 
     const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-    if(!isPasswordCorrect) return next(createError(400, "Wrong password or username"));
+    if (!isPasswordCorrect) return next(createError(400, "Wrong password or username"));
 
     const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT);
 
     const { password, isAdmin, ...otherDetails } = user._doc;
 
-    res.cookie("access_token", token, { httpOnly: true }).status(201).json({...otherDetails});
+    res.cookie("access_token", token, { httpOnly: true }).status(201).json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
