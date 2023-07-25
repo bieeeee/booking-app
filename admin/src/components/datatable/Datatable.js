fcb6b992a6dc75897b7689b6edfccb7183dea7fd
hanbyeol kwon <janviertrois@naver.com>
2023-07-25 11:45:22 -0400

    Add delete function to datatable

--- admin/src/components/datatable/Datatable.js
+++ admin/src/components/datatable/Datatable.js
@@ -2,20 +2,26 @@ import "./datatable.scss";
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from "../../datatablesource.js";
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 function Datatable() {
+  const [data, setData] = useState(userRows);
+  const handleDelete = (id) => {
+    setData(data.filter(item => item.id !== id));
+  }
+
   const actionColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ color: "inherit", textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</div>
           </div>
         )
       }
@@ -34,7 +40,7 @@ function Datatable() {
         </Link>
       </div>
       <DataGrid
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         initialState={{
           pagination: {
