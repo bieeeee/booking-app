b577f113f5925ec21172696d845a1a4c7dee787d
hanbyeol kwon <janviertrois@naver.com>
2023-07-28 17:49:24 -0400

    Set room hotelId path, post new room

---
+++ admin/src/pages/newRoom/NewRoom.js
@@ -0,0 +1,78 @@
+import "./newRoom.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import axios from "axios";
+
+function NewRoom({ inputs, title }) {
+  const [info, setInfo] = useState({});
+  const [hotelId, setHotelId] = useState(undefined);
+  const [rooms, setRooms] = useState([]);
+
+  const { data, loading, error } = useFetch("/hotels");
+
+  const handleInfo = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const roomNumbers = rooms.split(",").map((roomNumber) => (
+      { number: roomNumber }
+    ))
+    try {
+      const newRoom = { ...info, roomNumbers }
+      await axios.post(`/rooms/${hotelId}`, newRoom)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          {/* <div className="left">
+            <img
+              src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
+              alt=""
+            />
+          </div> */}
+          <div className="right">
+            <form>
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input id={input.id} type={input.type} placeholder={input.placeholder} onChange={handleInfo} />
+                </div>
+              ))}
+              <div className="formInput">
+                <label>Rooms</label>
+                <textarea onChange={e => setRooms(e.target.value)} placeholder="Give comma between room numbers." />
+              </div>
+              <div className="formInput" >
+                <label>Choose a hotel</label>
+                <select id="hotelId" onChange={e => setHotelId(e.target.value)}>
+                  {loading ? "Loading..." : data && data.map((hotel) => (
+                    <option key={hotel._id} value={hotel._id}>{hotel.name}</option>
+                  ))}
+                </select>
+              </div>
+              <button onClick={handleClick}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NewRoom
